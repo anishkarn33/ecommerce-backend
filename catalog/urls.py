b11bb2032b76bb7746e9a392003ae47fb176e006
anishkarn33 <anish.karna133@gmail.com>
2022-11-20 12:03:33 +0545

    first commit

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path, include
 from django.contrib.auth.models import User
-from rest_framework import routers, serializers, viewsets
+from rest_framework import routers, serializers, viewsets 
 
 # Serializers define the API representation.
 class CatalogSerializers(serializers.ModelSerializer):
